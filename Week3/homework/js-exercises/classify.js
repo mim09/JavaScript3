4061522111b4cf3b71dd9f4034c390fd8b1ff83d
Ayda <aydahagos@yahoo.com>
2020-11-17 14:15:48 +0100

    class edited after review

--- Week3/homework/js-exercises/classify.js
+++ Week3/homework/js-exercises/classify.js
@@ -1,16 +1,6 @@
-// Abdulkareem is a 35 year old man, that lives in Riyadh.
-// He has a wife and 3 children.As a day job he's a construction worker, that makes houses.
-// He likes to eat dates and smoke water pipe.
-
-// Abdulkareem has a horse, named Adel.
-// The horse is 15 years old and has the color brown.
-// Usually the horse eats grass or helps transport materials for Abdulkareem.
-
-// And they lived happily ever after!
 
 class Person {
-  constructor(name, what, age, city, children, job, eats) {
-    this.what = what;
+  constructor(name, age, city, children, job, eats) {
     this.name = name;
     this.age = age;
     this.city = city;
@@ -19,26 +9,29 @@ class Person {
     this.eats = eats;
   }
   get person() {
-    return `${this.name} is a ${this.age} year old ${this.what}, that lives in ${this.city}.
+    return `${this.name} is a man year old ${this.what}, that lives in ${this.city}.
         He has a wife and ${this.children} children.
         As a day job he's a ${this.job} worker, that makes houses.
         He likes to ${this.eats} and smoke water pipe.`
   }
 }
 
-class Horse extends Person {
+class Horse {
   constructor(what, name, age, color, eats) {
-    super(what, name, age, eats)
-    this.color = color
+    this.what = what;
+    this.name = name;
+    this.age = age;
+    this.color = color;
+    this.eats = eats;
   }
   get horse() {
-    return `${person1.name} has a ${this.what}, named ${this.name}.
+    return `Abdulkerim has a ${this.what}, named ${this.name}.
   The horse is ${this.age} years old and has the color ${this.color}.
-  Usually the horse eats ${this.eats} or helps transport materials for ${person1.name}.`
+  Usually the horse eats ${this.eats} or helps transport materials for Abdulkerim. \n And they lived happily ever after!`
   }
 
 }
-const person1 = new Person('Abdulkerim', 'man', '35', 'riyadh', '3', 'construction', 'dates');
+const person1 = new Person('Abdulkerim', '35', 'riyadh', '3', 'construction', 'dates');
 console.log(person1.person);
 const horse1 = new Horse('horse', 'Adel', '15', 'brown', 'grass');
 console.log(horse1.horse);
